7cc2a86c627e736586e3cba5f7803b50f87115ac
craftmine1000 <craftmine1000@gmail.com>
2021-08-02 07:04:19 +0200

    fix labels

--- jukebox/sample.py
+++ jukebox/sample.py
@@ -101,12 +101,14 @@ def sample_level(zs, labels, sampling_kwargs, level, prior, total_length, hop_le
             for start in range(0, total_length, hop_length):
                 batches = []
                 for batch in range(hps.n_samples):
+                    label = labels[batch]
                     tz = [t.zeros((0,)) for _ in range(hps.levels)]
 
                     tz[level + 1] = zs[level + 1][batch, start // 4 : (start + hop_length) // 4].reshape((-1, prior.n_ctx // 4)) #ToDo: change these 4s to hps.downcond or whatever its named
-                    tz[level] = t.zeros((tz[level + 1].shape[0], 0), dtype=zs[level].dtype)
+                    new_batch = tz[level + 1].shape[0]
+                    tz[level] = t.zeros((new_batch, 0), dtype=zs[level].dtype)
 
-                    tz = sample_single_window(tz, labels, sampling_kwargs, level, prior, 0, hps)
+                    tz = sample_single_window(tz, [label] * new_batch, sampling_kwargs, level, prior, 0, hps)
                     
                     batches.append(tz[level].reshape((-1,)))
                 zs[level] = t.cat((zs[level], t.stack(batches, dim=0)), dim=1)
